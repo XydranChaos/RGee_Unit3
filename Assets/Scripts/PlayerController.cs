6497d26b6cffdb24ba98239277c27f85b7b44789
XydranChaos <78708475+XydranChaos@users.noreply.github.com>
2021-04-03 12:02:13 -0700

    Finished Unit 3 Lesson C

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,22 +9,27 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     private bool onGround = true;
     public bool gameOver = false;
+
+    private Animator animPlayer;
     void Start()
     {
         rbPlayer = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
-        
+
+        animPlayer = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //conditions met to Jump
         bool spaceDown = Input.GetKeyDown(KeyCode.Space);
-        if(spaceDown && onGround)
+        if(spaceDown && onGround && !gameOver)
         {
             //jump
             rbPlayer.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             onGround = false;
+            animPlayer.SetTrigger("Jump_trig");
         }
     }
 
@@ -34,10 +39,13 @@ public class PlayerController : MonoBehaviour
         {
             onGround = true;
         }
+        //game is over with this condition met
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over!");
             gameOver = true;
+            animPlayer.SetBool("Death_b", true);
+            animPlayer.SetInteger("DeathType_int", 2);
         }
     }
 }
