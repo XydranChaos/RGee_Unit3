026e0d3e7e206b63c5b1af592691e191dabce0f9
XydranChaos <78708475+XydranChaos@users.noreply.github.com>
2021-04-03 10:08:02 -0700

    Finished Unit 3 Lesson A

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rbPlayer;
+    public float gravityModifier;
+    public float jumpForce;
+    private bool onGround = true;
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool spaceDown = Input.GetKeyDown(KeyCode.Space);
+        if(spaceDown && onGround)
+        {
+            //jump
+            rbPlayer.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            onGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        onGround = true;
+    }
+}
